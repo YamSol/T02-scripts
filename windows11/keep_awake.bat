8fea5088dfe76fc658e05ba7d9ab119b2f94a52b
YamSol <yambertamini.sol@gmail.com>
2026-06-05 22:06:22 -0300

    versao simplificada do script keep-awake

--- windows11/keep_awake.bat
+++ windows11/keep_awake.bat
@@ -1,13 +1,6 @@
 @echo off
 title Keep Awake
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "Add-Type -AssemblyName System.Windows.Forms; ^
-   $sig = '[DllImport(\"user32.dll\")] public static extern void mouse_event(int f, int x, int y, int d, int e);'; ^
-   $m = Add-Type -MemberDefinition $sig -Name Mouse -Namespace W -PassThru; ^
-   while ($true) { ^
-     $pos = [System.Windows.Forms.Cursor]::Position; ^
-     $m::mouse_event(1, 1, 0, 0, 0); ^
-     $m::mouse_event(1, -1, 0, 0, 0); ^
-     [System.Windows.Forms.Cursor]::Position = $pos; ^
-     Start-Sleep -Seconds 60 ^
-   }"
+:loop
+powershell -NoProfile -Command "$ws = New-Object -ComObject WScript.Shell; $ws.SendKeys([char]0)"
+timeout /t 60 /nobreak >nul
+goto loop
